3343c54a78788797391361f7e2740a2e4ce944f6
Radu <simaradu25@gmail.com>
2021-03-11 13:11:18 +0200

    haideeee

---
+++ Command.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Radu on 3/11/21.
+//
+
+#include "Command.h"
---
+++ cmake-build-debug/CMakeFiles/PA_chess_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA_chess_game.dir/main.cpp.o"
+  "PA_chess_game"
+  "PA_chess_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA_chess_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
