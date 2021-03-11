9a98ec9b1105f751141ddb0c90168fbcef80c3b4
Ciprian <cipri.constantinescu@gmail.com>
2021-03-11 12:35:56 +0200

    modif

--- main.cpp
+++ main.cpp
@@ -14,12 +14,19 @@ int main() {
 
     while(strcmp(command, "quit") != 0) {
         if(strcmp(command, "protover 2") == 0) {
-            cout<<"feature sigint=0 san=0 name=\"myChess\""<<endl;
-            cout.flush();
+            cout<<"feature sigint=0 san=0 name=\"LookingAtTheCeiling\""<<endl;
         }
         else if(strcmp(command, "e2e4") == 0) {
             cout<<"move e7e5"<<endl;
-            cout.flush();
+        }
+        else if(strcmp(command, "f1c4") == 0) {
+            cout<<"move b8c6"<<endl;
+        }
+        else if(strcmp(command, "d1h5") == 0) {
+            cout<<"move g8f6"<<endl;
+        }
+        else if(strcmp(command, "h5f7") == 0) {
+            cout<<"resign"<<endl;
         }
         myfile << command << endl;
         cin.getline(command,100);
